119b123f63297e05b206e65debf86d436f4cc129
liampc <kathliampcarlos@gmail.com>
2021-02-17 19:19:48 +0800

    convert section to card component

---
+++ src/components/Card.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+import Input from './Input'
+
+function Card(props){
+
+    let inputs = props.inputs.map(item => {
+        return <Input name={item.name} type={item.type} />
+    })
+
+    return (
+        <section className="card">
+            <h2>{props.header}</h2>
+            {inputs}
+        </section>
+    )
+}
+
+export default Card
--- src/layout/main/Main.js
+++ src/layout/main/Main.js
@@ -1,19 +1,27 @@
 import React from 'react'
 
 import './_main.scss'
+import Card from  "../../components/Card"
 import Input from '../../components/Input'
 
 function Section(props){
 
+    const basicInfo = [
+        {name: 'Name', type: 'text' }, 
+        {name: 'Email', type: 'email'}
+    ]
+
+    const education = [
+        {name: 'School', type: 'text'},
+        {name: 'Year', type: 'number'}
+    ]
+
+
 
     return (
         <main className="main">
-            <section className="main__card">
-                <h2>Basic Info</h2>
-                <Input name={"Name"} type={"text"} />
-                <Input name={"Email"} type={"email"} />
-                <Input name={"Number"} type={"number"} />
-            </section>
+            <Card header={"Basic Info"} inputs={basicInfo} />
+            <Card header={"Education"} inputs={education}/>
         </main>
     )
 }
