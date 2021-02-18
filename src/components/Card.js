8c024fa3384c6b5efcab7905fcf48dfb77cd73da
liampc <kathliampcarlos@gmail.com>
2021-02-18 10:14:32 +0800

    add handleClick to card

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,18 +1,27 @@
-import React from 'react';
+import React, {useState} from 'react';
 import uniqid from 'uniqid';
 
 import Input from './Input'
 
 function Card(props){
 
-    let inputs = props.inputs.map(item => {
+    const [list, setList] = useState(props.inputs)
+
+    
+    let display = list.map(item => {
         return <Input name={item.name} type={item.type} key={uniqid()} />
     })
+
+    function handleClick(){
+        setList(prevState => prevState.concat(props.inputs))
+    }
+
 
     return (
         <section className="card" id={props.header}>
             <h2>{props.header}</h2>
-            {inputs}
+            {display}
+            <span onClick={handleClick}>+</span>
         </section>
     )
 }
